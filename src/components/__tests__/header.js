4a22c84d435800b7d2943ce9ab7010a1bc824c24
unknown <adam.m.bodie@gmail.com>
2020-01-21 11:11:47 -0800

    Added jest, setup testing

---
+++ src/components/__tests__/header.js
@@ -0,0 +1,24 @@
+import React from "react"
+import renderer from "react-test-renderer"
+import { StaticQuery } from "gatsby"
+import Header from "../header"
+
+
+//TODO: image.js not testing correctly
+beforeEach(() => {
+  StaticQuery.mockImplementationOnce(({ render }) =>
+    render({
+      site: {
+        siteMetadata: {
+          title: `Default Starter`,
+        },
+      }
+    })
+  )
+})
+
+describe("Header", () =>
+  it("renders correctly", () => {
+    const tree = renderer.create(<Header />).toJSON()
+    expect(tree).toMatchSnapshot()
+  }))
